c1985813ddceecf1ded9baa2808c369e8e96b497
Lee Jongyoung <leejongyoung@icloud.com>
2022-06-06 12:34:45 +0900

    Fetch data from excel file

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
+def fetch_data_from_excel_file(sheet):
+    name_list = []
+    registration_num_list = []
+    address_list = []
+    division_list = []
+    position_list = []
+    employment_period_list = []
+    dutiees_list = []
+
+    for i in range(1, sheet.max_row + 1):
+        name_list.append(sheet.cell(i, 1).value)
+        registration_num_list.append(sheet.cell(i, 2).value)
+        address_list.append(sheet.cell(i, 3).value)
+        division_list.append(sheet.cell(i, 4).value)
+        position_list.append(sheet.cell(i, 5).value)
+        employment_period_list.append(sheet.cell(i, 6).value)
+        dutiees_list.append(sheet.cell(i, 7).value)
+    
+    # print
+    print(name_list)
+    print(registration_num_list)
+    print(address_list)
+    print(division_list)
+    print(position_list)
+    print(employment_period_list)
+    print(dutiees_list)
+
 def generator_excel_file():
     df = pd.DataFrame([["성명", "주민등록번호", "주소", "소속", "직위", "재직기간", "담당업무"],
                     ["김민준", "900506-1581223", "서울특별시 강남구 역삼동 824-25", "R&D", "대리", "2020.10.14~2022.06.06", "소프트웨어엔지니어"],
@@ -18,5 +45,7 @@ except:
     wb = Workbook()
     sheet = wb.active
     generator_excel_file()
+
+fetch_data_from_excel_file(sheet)
 
 wb.save("personal_info.xlsx")
